65f421772474e53b563a160aa96cbf8d7f3675e0
callac <callac@users.noreply.github.com>
2024-02-16 17:25:12 +0000

    Deploying to deploy-pages from @ callac/callac-docs@34efe65f94b03fb7f23dd52af7d253bfa93f7244 🚀

--- docs/.vitepress/socialLinks.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultTheme } from 'vitepress';
-
-// https://vitepress.dev/reference/default-theme-config#sociallinks
-export const socialLinks: DefaultTheme.SocialLink[] = [
-  { icon: "github", link: "callac-docs/docs/.vitepress/info.ts" },
-];
